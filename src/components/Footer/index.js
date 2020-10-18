e3b7bdf630f3fae71bd4de3f63f00f6a50ca5a48
AkremDev <akremdev@pop-os.localdomain>
2020-10-18 23:20:10 +0100

    make it better

---
+++ src/components/Footer/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { animateScroll as scroll } from "react-scroll";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaYoutube,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterLink,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  SocialMedia,
+  SocialMediaWrap,
+  WebsiteRights,
+} from "./FooterElements";
+
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us </FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos </FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us </FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Socail Media </FooterLinkTitle>
+              <FooterLink to="/">Instagram</FooterLink>
+              <FooterLink to="/">Facebook</FooterLink>
+              <FooterLink to="/">Youtube</FooterLink>
+              <FooterLink to="/">Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/" onClick={toggleHome}>
+              Lak
+            </SocialLogo>
+            <WebsiteRights>
+              Lak © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Signin/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Container,
+  Form,
+  FormButton,
+  FormContent,
+  FormH1,
+  FormInput,
+  FormLabel,
+  FormWrap,
+  Icon,
+  Text,
+} from "./SigninElements";
+
+const Signin = () => {
+  return (
+    <>
+      <Container>
+        <FormWrap>
+          <Icon to="/">Lak</Icon>
+          <FormContent>
+            <Form action="#">
+              <FormH1>Sign in to your account</FormH1>
+              <FormLabel htmlFor="for">Email</FormLabel>
+              <FormInput type="email" required />
+              <FormLabel htmlFor="for">Password</FormLabel>
+              <FormInput type="password" required />
+              <FormButton type="submit">Continue</FormButton>
+              <Text>Forgot Password</Text>
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
+    </>
+  );
+};
+
+export default Signin;
